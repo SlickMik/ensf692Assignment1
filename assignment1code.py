e63db9f4052a59c646089113716cb85890050d7d
Ibrahim Khan <kickflip206@gmail.com>
2025-05-11 19:11:02 -0600

    Update Python & README, Add Screenshots

---
+++ assignment1code.py
@@ -0,0 +1,9 @@
+# ENSF 592 Spring 2023
+# Assignment 1 Starter Code
+# Add your name here!
+
+studentName = "Ibrahim Khan"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 592.")
+print("My best skill is web-development")
